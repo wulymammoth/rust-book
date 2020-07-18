68561a62b60030ce215de5f6dc9c79d6955a9992
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-07-18 14:30:12 -0700

    ch03 ex - temperature conversion

---
+++ temps/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+
+fn main() {
+    println!("Please enter a temperator followed by F (for Fahrenheit) and C (for Celsius)");
+    let mut temperature = String::new();
+    io::stdin()
+        .read_line(&mut temperature)
+        .expect("Failed to read line");
+    let temperature: f32 = match temperature.trim().parse() {
+        Ok(t) => t,
+        Err(_) => panic!("Please, only enter numbers")
+    };
+    println!("Please enter the unit you want to convert this from");
+    let mut unit = String::new();
+    io::stdin()
+        .read_line(&mut unit)
+        .expect("Failed to read line");
+    let unit = unit.trim(); // shadowed
+    println!("Your entry of {}{} is {}", temperature, unit, convert(temperature, &unit));
+}
+
+fn convert(temp: f32, unit: &str) -> f32 {
+    let unit = unit.to_lowercase();
+    if unit == "c" { return (temp * 9.0 / 5.0) + 32.0 }
+    if unit == "f" { return (temp - 32.0) * 5.0 / 9.0 }
+    panic!("invalid unit");
+}
