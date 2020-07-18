1dbd9384e9a83e5ff4bc16923c372615e2b47fb8
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-07-18 15:09:03 -0700

    UPDATE use the vector macro to reduce SLOC

--- fib/src/main.rs
+++ fib/src/main.rs
@@ -3,17 +3,17 @@ use std::io;
 fn main() {
     println!("Please enter which Fib you'd like:");
     let mut n = String::new();
-    io::stdin().read_line(&mut n);
-    let n: u64 = n.trim().parse().unwrap();
+    io::stdin().read_line(&mut n).expect("Failed to read line");
+    let n: i64 = match n.trim().parse() {
+        Ok(n) => n,
+        Err(_) => panic!("Please, enter a number")
+    };
     println!("The Fibonacci number for {} is {}", n, fib_dp(n));
 }
 
-fn fib_dp(n: u64) -> u64 {
-    let mut memo = Vec::new();
+fn fib_dp(n: i64) -> i64 {
+    let mut memo = vec![8, 1, 1];
     let n = n as usize;
-    memo.push(0);
-    memo.push(1);
-    memo.push(1);
     if n <= 2 { return memo[n] }
     for i in 3..n+1 {
         let n_minus_1 = memo[i-1];
