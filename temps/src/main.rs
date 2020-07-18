ea3ac4fb7d4aa55f8e0887d9c204d8c2b1aaf60e
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-07-18 14:38:05 -0700

    UPDATE employ the use of  to make code more terse

--- temps/src/main.rs
+++ temps/src/main.rs
@@ -6,22 +6,28 @@ fn main() {
     io::stdin()
         .read_line(&mut temperature)
         .expect("Failed to read line");
-    let temperature: f32 = match temperature.trim().parse() {
-        Ok(t) => t,
-        Err(_) => panic!("Please, only enter numbers")
-    };
+    let temperature: f32 = temperature.trim().parse().unwrap();
     println!("Please enter the unit you want to convert this from");
     let mut unit = String::new();
     io::stdin()
         .read_line(&mut unit)
         .expect("Failed to read line");
     let unit = unit.trim(); // shadowed
-    println!("Your entry of {}{} is {}", temperature, unit, convert(temperature, &unit));
+    println!(
+        "Your entry of {}{} is {}",
+        temperature,
+        unit,
+        convert(temperature, &unit)
+    );
 }
 
 fn convert(temp: f32, unit: &str) -> f32 {
     let unit = unit.to_lowercase();
-    if unit == "c" { return (temp * 9.0 / 5.0) + 32.0 }
-    if unit == "f" { return (temp - 32.0) * 5.0 / 9.0 }
+    if unit == "c" {
+        return (temp * 9.0 / 5.0) + 32.0;
+    }
+    if unit == "f" {
+        return (temp - 32.0) * 5.0 / 9.0;
+    }
     panic!("invalid unit");
 }
