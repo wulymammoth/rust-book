84a2abb50b2c68efcb21f9d8ed6b3f62a0527a19
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-07-20 19:52:57 -0700

    ownership - string slices instead of string values is more flexible (can operate on both &String and &str)

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,14 +1,16 @@
 fn main() {
     let mut s = String::from("hello world");
     let word = first_word(&s); // results in 5 (where the space is)
+    println!("first word: {}", word);
     s.clear(); // this empties the String. making it ""
 
     // word still has the value of 5 here but is only relevant in the context of our original
     // string
+    println!("cleared word {}", s);
 }
 
 // return index of the end of the word
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
