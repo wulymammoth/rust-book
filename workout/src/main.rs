9c3b54472da36515e7ff986acb59f2370bc1e5c4
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-09-11 23:25:07 -0700

    refactor cacher to support multiple cached values
    - use of a hashmap instead of a scalar field `value`

--- workout/src/main.rs
+++ workout/src/main.rs
@@ -1,11 +1,11 @@
-use std::{thread, time::Duration};
+use std::{collections::HashMap, thread, time::Duration};
 
 struct Cacher<T>
 where
     T: Fn(u32) -> u32,
 {
     calculation: T,
-    value: Option<u32>,
+    values: HashMap<u32, u32>,
 }
 
 impl<T> Cacher<T>
@@ -15,16 +15,16 @@ where
     fn new(calculation: T) -> Cacher<T> {
         Cacher {
             calculation,
-            value: None,
+            values: HashMap::new(),
         }
     }
 
     fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
+        match self.values.get(&arg) {
+            Some(v) => *v,
             None => {
                 let v = (self.calculation)(arg);
-                self.value = Some(v);
+                self.values.insert(arg, v);
                 v
             }
         }
@@ -54,4 +54,19 @@ fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
     generate_workout(simulated_user_specified_value, simulated_random_number);
+}
+
+#[cfg(test)]
+ mod tests {
+     use super::*;
+
+     #[test]
+     fn call_with_different_values() {
+         let mut c = Cacher::new(|a| a);
+         let v1 = c.value(1);
+         let v2 = c.value(2);
+
+         assert_eq!(v1, 1);
+         assert_eq!(v2, 2);
+     }
 }
