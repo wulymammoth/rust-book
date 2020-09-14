3d336138bc9a83935d5eb13edf684c0580fa79a4
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-09-14 13:29:15 -0700

    iterators

--- workout/src/main.rs
+++ workout/src/main.rs
@@ -45,7 +45,10 @@ fn generate_workout(intensity: u32, random_number: u32) {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!("Today, run for {} minutes!", expensive_result.value(intensity));
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
         }
     }
 }
@@ -65,6 +68,15 @@ fn move_closure() {
     assert!(equal_to_x(y));
 }
 
+fn iterators() {
+    // iterators are lazy
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+    for val in v1_iter {
+        println!("got: {}", val);
+    }
+}
+
 fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
@@ -75,19 +87,33 @@ fn main() {
 
     println!("move closure example");
     move_closure();
+
+    println!("\niterators");
+    iterators();
 }
 
 #[cfg(test)]
- mod tests {
-     use super::*;
+mod tests {
+    use super::*;
 
-     #[test]
-     fn call_with_different_values() {
-         let mut c = Cacher::new(|a| a);
-         let v1 = c.value(1);
-         let v2 = c.value(2);
+    #[test]
+    fn call_with_different_values() {
+        let mut c = Cacher::new(|a| a);
+        let v1 = c.value(1);
+        let v2 = c.value(2);
 
-         assert_eq!(v1, 1);
-         assert_eq!(v2, 2);
-     }
+        assert_eq!(v1, 1);
+        assert_eq!(v2, 2);
+    }
+
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1, 2, 3];
+        let mut v1_iter = v1.iter();
+
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+    }
 }
