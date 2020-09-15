cab0485f058746716dc850968de6bae2256e3c3c
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-09-15 10:44:38 -0700

    use a for-loop to do assertions

--- workout/src/lib.rs
+++ workout/src/lib.rs
@@ -77,10 +77,11 @@ impl Iterator for Counter {
 fn ceiling_next_directly() {
     let mut counter = Counter::new();
 
-    assert_eq!(counter.next(), Some(1));
-    assert_eq!(counter.next(), Some(2));
-    assert_eq!(counter.next(), Some(3));
-    assert_eq!(counter.next(), Some(4));
-    assert_eq!(counter.next(), Some(5));
-    assert_eq!(counter.next(), None);
+    for i in 1..6 {
+        if i <= 5 {
+            assert_eq!(counter.next(), Some(i));
+        } else {
+            assert_eq!(counter.next(), None);
+        }
+    }
 }
