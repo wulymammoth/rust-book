1ebe69dc81142a2b5be1013fc8b0fae7d198bb31
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-09-15 13:25:47 -0700

    more improvements with iterators to a more functional style

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -47,13 +47,10 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line)
-        }
-    }
-    result
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
