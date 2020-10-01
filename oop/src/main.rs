e79e0430e3d6fb096f6c4ac80b142eaef7e9fecd
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-09-30 20:45:13 -0700

    OOP : state patterns - encoding states and behavior as types

--- oop/src/main.rs
+++ oop/src/main.rs
@@ -1,4 +1,4 @@
-use oop::{AveragedCollection, Draw, Screen};
+use oop::{AveragedCollection, Draw, Post, Screen};
 
 pub struct Button {
     pub width: u32,
@@ -13,6 +13,7 @@ impl Draw for Button {
 }
 
 // NOTE: could have been an extension implemented by someone else
+#[allow(dead_code)]
 pub struct SelectBox {
     width: u32,
     height: u32,
@@ -50,26 +51,41 @@ fn trait_bound_objects() {
 }
 
 fn state_pattern() {
-    use oop::Post;
+    println!("\n----- 17.3 OOP : implementing an object-oriented design pattern (state pattern)\n");
 
-    fn main() {
-        let mut post = Post::new();
+    let mut post = Post::new();
 
-        post.add_text("I ate a salad for lunch today");
-        assert_eq!("", post.content());
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
 
-        post.request_review();
-        assert_eq!("", post.content());
+    post.request_review();
+    assert_eq!("", post.content());
 
-        post.approve();
-        assert_eq!("", post.content());
-    }
+    post.approve();
+    assert_eq!("", post.content());
+}
+
+fn encoding_states_and_behavior_as_types() {
+    println!("\n----- 17.3 OOP : implementing an object-oriented design pattern (encoding states and behavior types)\n");
+
+    use oop::redux;
+
+    let mut post = redux::Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
 
 fn main() {
     let collection = AveragedCollection::new();
-    println!("this is an averaged collection:\n\n{:#?}", collection);
+    println!("this is an averaged collection:\n{:#?}", collection);
 
     trait_bound_objects();
     state_pattern();
+    encoding_states_and_behavior_as_types();
 }
