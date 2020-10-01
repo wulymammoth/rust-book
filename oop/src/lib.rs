de0e460eceaed42400d6d0a799e9cb289c1c8d70
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-09-30 20:45:13 -0700

    OOP : state patterns - encoding states and behavior as types

--- oop/src/lib.rs
+++ oop/src/lib.rs
@@ -92,6 +92,7 @@ impl Post {
     }
 }
 
+#[allow(unused_variables)]
 trait State {
     fn request_review(self: Box<Self>) -> Box<dyn State>;
     fn approve(self: Box<Self>) -> Box<dyn State>;
@@ -133,5 +134,53 @@ impl State for Published {
 
     fn approve(self: Box<Self>) -> Box<dyn State> {
         self
+    }
+}
+
+pub mod redux {
+    // represented a published post
+    pub struct Post {
+        content: String,
+    }
+
+    // repesents an unpublished post
+    pub struct DraftPost {
+        content: String,
+    }
+
+    impl Post {
+        pub fn new() -> DraftPost {
+            DraftPost {
+                content: String::new(),
+            }
+        }
+
+        pub fn content(&self) -> &str {
+            &self.content
+        }
+    }
+
+    impl DraftPost {
+        pub fn add_text(&mut self, text: &str) {
+            self.content.push_str(text);
+        }
+
+        pub fn request_review(self) -> PendingReviewPost {
+            PendingReviewPost {
+                content: self.content,
+            }
+        }
+    }
+
+    pub struct PendingReviewPost {
+        content: String,
+    }
+
+    impl PendingReviewPost {
+        pub fn approve(self) -> Post {
+            Post {
+                content: self.content,
+            }
+        }
     }
 }
