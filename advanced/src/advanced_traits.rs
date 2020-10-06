f33eb6dfe02beb731fa5168229d6dc48a2924c9f
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-10-06 10:39:26 -0700

    advanced features : advanced traits
    - WIP default generic type parameters and operator overloading
    - specifying placeholder types in trait defs with associated types

--- advanced/src/advanced_traits.rs
+++ advanced/src/advanced_traits.rs
@@ -3,10 +3,15 @@ pub fn main() {
     specifying_placeholder_types_in_trait_defs_with_associated_types();
 }
 
+fn default_generic_type_parameters_and_operator_overloading() {}
+
 fn specifying_placeholder_types_in_trait_defs_with_associated_types() {
     pub trait Iterator {
-        type Item;
+        type Item; // placeholder type
 
+        // implementors of Iterator must specify a concrete type for Item
+        // NOTE: with associated types, we don't need to annotate types because we can't implement
+        // a trait on a type multiple times
         fn next(&mut self) -> Option<Self::Item>;
     }
 }
